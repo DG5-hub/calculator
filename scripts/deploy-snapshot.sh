7d6f04a646ce882af0bc47e357d4f0d42a67eecd
user1 user1 <user1@gitlab.com>
2021-11-12 20:22:09 +0100

    update environments

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -12,21 +12,22 @@ PRODUCT_SNAPSHOT_NAME=calculator.war
 RESOURCE_NAME=calculator.war
 
 
+
 # Shutdown Tomcat
-sh $APACHE_BIN/shutdown.sh
+sudo sh $APACHE_BIN/shutdown.sh
 
 
 # Deploy generated SNAPSHOT into the dev-env  
-sudo cp /vagrant_target/$PRODUCT_SNAPSHOT_NAME $APACHE_WEBAPPS
-cd $APACHE_WEBAPPS
+# sudo cp /vagrant_target/$PRODUCT_SNAPSHOT_NAME $APACHE_WEBAPPS
+#cd $APACHE_WEBAPPS
 
 #mkdir $RESOURCE_NAME
 #cp $PRODUCT_SNAPSHOT_NAME $RESOURCE_NAME
-sudo mv $PRODUCT_SNAPSHOT_NAME $RESOURCE_NAME
-sudo chown tomcat:tomcat $RESOURCE_NAME
+# sudo mv $PRODUCT_SNAPSHOT_NAME $RESOURCE_NAME
+sudo chown tomcat:tomcat $APACHE_WEBAPPS/$RESOURCE_NAME
 #rm $PRODUCT_SNAPSHOT_NAME
 #cd $RESOURCE_NAME
 #sudo jar -xvf $PRODUCT_SNAPSHOT_NAME.war
 
 # Start up Tomcat
-sh $APACHE_BIN/startup.sh
+sudo sh $APACHE_BIN/startup.sh
